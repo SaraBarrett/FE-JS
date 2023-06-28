5be993322096dd312d34506bd62fdb9294e5b728
SaraBarrett <sara.c.mon@gmail.com>
2023-06-28 19:44:20 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+//alert('hello world');
+
+//let myName = 'Sara';
+//let myLuckyNumber = 16;
+
+/*alert(myName + ' e o meu número preferido é'+ myLuckyNumber);
+
+console.log(myName);
+
+
+const message = '   TASTE THE RAINBOW ';
+
+let whisper = message.toLowerCase().trim();
+
+console.log(whisper);*/
+
+
+const word ='skateboard';
+
+let facialHair = word.slice(5);
+
+facialHair = facialHair.replace('o','e');
+console.log(facialHair);
